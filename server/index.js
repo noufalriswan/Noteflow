967b77cdf40ce1cf72dde9146b5c60918c3558dc
Noufal Riswan <noufalrizwan2008@gmail.com>
2026-07-02 16:43:20 +0530

    Fix MongoDB Atlas connection

--- server/index.js
+++ server/index.js
@@ -12,11 +12,14 @@ app.use(express.json());
 
 app.use("/api/tasks", taskRoutes);
 
-mongoose.connect("mongodb://127.0.0.1:27017/Tasks")
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("✅ MongoDB Connected");
-    app.listen(5000, () => {
-      console.log("Server is running on port 5000");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
   })
   .catch((err) => {
